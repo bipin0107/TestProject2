6e3704fc92978b3799c911e3217a591d9c885d6f
bipin0107 <bipinsingh007@gmail.com>
2025-11-16 11:30:40 +0530

    Added Class1

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2
+{
+    internal class Class1
+    {
+        Console.WriteLine("Class 1");
+    }
+}
