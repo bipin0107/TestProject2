5aa31635f7c3a334e8b8e0a52f3ef931171cf31f
bipin0107 <bipinsingh007@gmail.com>
2025-11-16 12:57:46 +0530

    Add additional console output for Class1
    Adding details

--- Class1.cs
+++ Class1.cs
@@ -9,5 +9,11 @@ namespace TestProject2
     internal class Class1
     {
         Console.WriteLine("Class 1");
+                Console.WriteLine("Class 2");
+        Console.WriteLine("Class 3");
+        Console.WriteLine("Class 4");
+        Console.WriteLine("Class 5");
+        Console.WriteLine("Class 6");
+
     }
 }
